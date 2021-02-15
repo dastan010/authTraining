10cc2f9e51fdefdbe78f9e66e5263dc1f950099a
d.akhmetov <d.akhmetov@small.kz>
2021-02-15 16:22:15 +0600

    init

---
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+class ApiAuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all());
+    }
+}
